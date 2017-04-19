ebd5e4fcb20a2307a549a96e0bfd82e088317cfe
Ken Bonny <bonny.ken@gmail.com>
2017-04-19 20:42:01 +0200

    Add speed test when inserting records.

---
+++ KenBonny.Experiment.UnitTests/InsertMillionCustomersSpeedTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using KenBonny.Experiment.ExperimentalLibrary;
+using Xunit;
+
+namespace KenBonny.Experiment.UnitTests
+{
+    public class InsertMillionCustomersSpeedTest
+    {
+        [Fact]
+        public void Speedtest_when_adding_a_million_customers_to_CustomerKeyedCollection()
+        {
+            var collection = new CustomerKeyedCollection();
+
+            for (int i = 0; i < 1_000_000; i++)
+            {
+                var customer = new Customer(Guid.NewGuid().ToString());
+                collection.Add(customer);
+            }
+        }
+
+        [Fact]
+        public void Speedtest_when_adding_a_million_customers_to_List()
+        {
+            var collection = new List<Customer>();
+
+            for (int i = 0; i < 1_000_000; i++)
+            {
+                var customer = new Customer(Guid.NewGuid().ToString());
+                collection.Add(customer);
+            }
+        }
+
+        [Fact]
+        public void Speedtest_when_adding_a_million_customers_to_Dictionary()
+        {
+            var collection = new Dictionary<string, Customer>();
+
+            for (int i = 0; i < 1_000_000; i++)
+            {
+                var customer = new Customer(Guid.NewGuid().ToString());
+                collection.Add(customer.Name, customer);
+            }
+        }
+    }
+}
